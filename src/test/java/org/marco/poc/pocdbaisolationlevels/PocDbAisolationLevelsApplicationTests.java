c38c05e327c1888812af9fb1a699a208ebd5a08e
Marco Antonio Béjar Villalba <marco.antonio.bejar.villalba@gmail.com>
2024-07-30 14:45:18 -0600

    Spring boot project

---
+++ src/test/java/org/marco/poc/pocdbaisolationlevels/PocDbAisolationLevelsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.marco.poc.pocdbaisolationlevels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocDbAisolationLevelsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
